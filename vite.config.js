8909c02dc5dbdeb59eec98c4b005ea7decd2ab1c
zaort <czafra.ortega@gmail.com>
2024-03-21 10:46:32 -0600

    congi updates for heroku

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  }
+  // Removed server configuration for deployment
 })
